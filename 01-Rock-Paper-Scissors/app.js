f745f2cdbb0db862873e05f868041c775f6954e8
Laxman Agrawal <laxman.singhal.com@gmail.com>
2026-08-31 16:21:18 +0530

    Update app.js

--- 01-Rock-Paper-Scissors/app.js
+++ 01-Rock-Paper-Scissors/app.js
@@ -2,10 +2,10 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
-const msg = document.querySelector("#msg");
+const msg = document.querySelector(".msg");
 
 const userScorePara = document.querySelector("#user-score");
-const compScorePara = document.querySelector("#computer-score");
+const compScorePara = document.querySelector(".computer-score");
 
 const genCompChoice = () => {
     const options = ["rock", "paper", "scissors"];
@@ -15,7 +15,7 @@ const genCompChoice = () => {
 
 const drawGame = () => {
     msg.innerText = "Game was Draw. Play again.";
-    msg.style.backgroundColor = "#081b31";
+    msg.style.backgroundColor = "&#081b31";
 };
 
 const showWinner = (userWin, userChoice, compChoice) => {
@@ -25,7 +25,7 @@ const showWinner = (userWin, userChoice, compChoice) => {
         msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
         msg.style.backgroundColor = "green";
     } else {
-        compScore++;
+        ++compScore;
         compScorePara.innerText = compScore;
         msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
         msg.style.backgroundColor = "red";
@@ -33,22 +33,17 @@ const showWinner = (userWin, userChoice, compChoice) => {
 };
 
 const playGame = (userChoice) => {
-  //Generate computer choice
     const compChoice = genCompChoice();
 
     if (userChoice === compChoice) {
-        //Draw Game
         drawGame();
     } else {
         let userWin = true;
         if (userChoice === "rock") {
-        //scissors, paper
         userWin = compChoice === "paper" ? false : true;
         } else if (userChoice === "paper") {
-        //rock, scissors
         userWin = compChoice === "scissors" ? false : true;
         } else {
-        //rock, paper
         userWin = compChoice === "rock" ? false : true;
         }
         showWinner(userWin, userChoice, compChoice);
