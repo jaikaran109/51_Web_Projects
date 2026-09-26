57e6abefb689ced601a3a9f00d64eb5eae9bc125
Rujula Sharma <sharmarujula123@gmail.com>
2026-09-26 19:51:01 +0530

    fix: add authentication to notes API

--- 21-Notes-Api_MongoDB/Backend/models/Note.js
+++ 21-Notes-Api_MongoDB/Backend/models/Note.js
@@ -1,17 +1,25 @@
-const mongoose = require('mongoose') ; 
+const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
-    title : {
-        required : true , 
-        type : String ,
-        trim : true 
-    } ,
-    desc : {
-        required : true , 
-        type : String ,
-        trim : true 
-    }
-})
+    title: {
+        required: true,
+        type: String,
+        trim: true
+    },
 
-const notes = mongoose.model("notes", noteSchema) ;
-module.exports = notes ;
+    desc: {
+        required: true,
+        type: String,
+        trim: true
+    },
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+const notes = mongoose.model('notes', noteSchema);
+
+module.exports = notes;
